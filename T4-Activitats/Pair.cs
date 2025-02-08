27b009800262afa5adf80a9b4d03aab87af9b472
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-08 13:11:30 +0100

    Exercice 1 Doned

---
+++ T4-Activitats/Pair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_Activitats
+{
+    public class Pair<T1,T2>
+    {
+        public T1 Ta1 { get; set; }
+        public T2 Ta2 { get; set; }
+
+        public Pair(T1 ta1,T2 ta2)
+        {
+            Ta1 = ta1;
+            Ta2 = ta2;
+        }
+        public override string ToString()
+        {
+            return $"{Ta1} {Ta2}";
+        }
+    }
+}
--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+namespace T4_Activitats
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pair<string, int> pair1 = new Pair<string, int>("pata1", 94742);
+            Console.WriteLine(pair1.ToString());
+            Pair<double, bool> pair2 = new Pair<double, bool>(45.44D, true);
+            Console.WriteLine(pair2.ToString());
+        }
+    }
+}
