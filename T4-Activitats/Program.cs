29f9622da86fd883e279aee2b624a3a6f1a12bda
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-08 13:20:40 +0100

    Exercice 2 Doned

--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Collections;
 namespace T4_Activitats
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            ArrayList arrayList = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                arrayList.Add(Console.ReadLine());
+            }
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
