0b55dc41444d479db8da0dbfc7ad091f4760a511
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-10 17:24:39 +0100

    Started exercice 5

--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -5,7 +5,20 @@ namespace T4_Activitats
     {
         public static void Main(string[] args)
         {
+            List<int> numeros = new List<int>();
+            string entrada;
+            bool flag = true;
+            while (flag)
+            {
+                entrada = Console.ReadLine();
+                if (string.IsNullOrEmpty(entrada))
+                    flag = false;
 
+            }
+            var parells = numeros.Where(n => n % 2 == 0);
+            foreach (int parell in parells){
+                Console.WriteLine(parell);
+            }
         }
     }
 }
