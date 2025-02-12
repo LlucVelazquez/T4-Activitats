ba91cade0241643fd9ad4d84fbad5e2fa9587489
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-12 12:28:15 +0100

    Doned Exercice 21

--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -3,8 +3,15 @@ namespace T4_Activitats
 {
     public class Program
     {
+        public delegate void Notificacio(string msg);
+        public static void MostrarMissatgeConsola(string msg) => Console.WriteLine(msg);
+        public static void MostrarMissatgeambEstrelles(string msg) => Console.WriteLine($"***{msg}");
         public static void Main(string[] args)
         {
+            Notificacio notificacio;
+            notificacio = MostrarMissatgeConsola;
+            notificacio += MostrarMissatgeambEstrelles;
+            notificacio("Soc una notificacio!");
 
         }
     }
