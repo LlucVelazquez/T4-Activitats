d51731e70514255a2b09ea31f32a94f8959af9ba
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-12 12:36:12 +0100

    Doned Exercice 24

--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -3,9 +3,14 @@ namespace T4_Activitats
 {
     public class Program
     {
+        public delegate void Notificacio(string msg);
+        public static void ExecutarAmbMetodeAnonim(Notificacio notificacio)
+        {
+            notificacio("Aixo es una notificacio");
+        }
         public static void Main(string[] args)
         {
-
+            ExecutarAmbMetodeAnonim((msg) => Console.WriteLine($"{msg}"));
         }
     }
 }
