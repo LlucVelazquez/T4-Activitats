3179a7c0df8f1a0e715fccbb64c080c3bfb8ad65
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-12 11:29:38 +0100

    Exercice 19

--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -3,9 +3,15 @@ namespace T4_Activitats
 {
     public class Program
     {
+        public delegate int Operacio(int a,int b);
+        public static int Multiplicar(int a,int b) { return a * b; }
+        public static int Divisio(int a, int b) => a / b;
         public static void Main(string[] args)
         {
-
+            Operacio multiplicar = Multiplicar;
+            Operacio divisio = Divisio;
+            Console.WriteLine(multiplicar(5,3));
+            Console.WriteLine(divisio(5,3));
         }
     }
 }
