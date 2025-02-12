556f5d2e3390e57f313d6cd25ab90a48e3d59a66
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-12 11:36:53 +0100

    Exercice 20

--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -3,9 +3,17 @@ namespace T4_Activitats
 {
     public class Program
     {
+        public delegate int Operacio(int a, int b);
+        public delegate void DelegatePrintOf(int a, int b);
+        public static void SumaPrint(int a, int b) { Console.WriteLine($"Suma {a+b}"); }
+        public static void RestaPrint(int a, int b) { Console.WriteLine($"Resta {a - b}"); }
+        public static int Multiplicar(int a, int b) { return a * b; }
+        public static int Divisio(int a, int b) => a / b;
         public static void Main(string[] args)
         {
-
+            DelegatePrintOf printOp = SumaPrint;
+            printOp += RestaPrint;
+            printOp(0,0);
         }
     }
 }
