811e0e3e72ab17077b30b6fc9968599ea48c4225
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-02-12 10:43:42 +0100

    Doned Exercice 6

--- T4-Activitats/Program.cs
+++ T4-Activitats/Program.cs
@@ -6,6 +6,11 @@ namespace T4_Activitats
         public static void Main(string[] args)
         {
 
+            string msg = "data: ";
+            Console.WriteLine(msg);
+            DateTime time = DateTime.Parse(Console.ReadLine());
+            TimeSpan result = time - DateTime.Today;
+            Console.WriteLine($"Dies que falten {result.Days}");
         }
     }
 }
